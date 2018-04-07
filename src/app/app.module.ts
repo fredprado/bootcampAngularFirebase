80f2f893dabef65ea67166db1b2ff3bdb4c77f9c
Frederico R. Prado <frederico.prado@venturus.org.br>
2018-04-07 13:34:37 -0300

    ex. 7 - Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListItemComponent } from './shopping-list/shopping-list-item/shopping-list-item.component';
 import { ShoppingListService } from './shopping-list.service';
-
+import { AboutComponent } from './about/about.component';
+import { APP_ROUTES } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingListItemComponent
+    ShoppingListItemComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
